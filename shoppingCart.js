d0751d4f2be43eebc180a8fd624cc6c786034d52
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-11 10:47:24 -0700

    work on addToCart functionality

--- shoppingCart.js
+++ shoppingCart.js
@@ -7,6 +7,8 @@ let shoppingCart = [];
 const IMAGE_URL = "https://dummyimage.com/210x130";
 const cartItemTemplate = document.querySelector("#cart-item-template");
 const cartItemContainer = document.querySelector("[data-cart-items]");
+const cartQuantity = document.querySelector("[data-cart-quantity]");
+const cartTotal = document.querySelector("[data-cart-total]");
 
 export function setupShoppingCart() {}
 
@@ -15,11 +17,25 @@ cartButton.addEventListener("click", () => {
 });
 
 export function addToCart(id) {
-  shoppingCart.push({ id: id, quantity: 1 });
+  const exsistingItem = shoppingCart.find((entry) => entry.id === id);
+  if (exsistingItem) {
+    exsistingItem.quantity++;
+  } else {
+    shoppingCart.push({ id: id, quantity: 1 });
+  }
   renderCart();
 }
 
 function renderCart() {
+  cartQuantity.innerText = shoppingCart.length;
+
+  const totalCents = shoppingCart.reduce((sum, entry) => {
+    const item = items.find((i) => entry.id === i.id);
+    return sum + item.priceCents * entry.quantity;
+  }, 0);
+
+  cartTotal.innerText = formatCurrency(totalCents / 100);
+
   cartItemContainer.innerHTML = "";
   shoppingCart.forEach((entry) => {
     const item = items.find((i) => entry.id === i.id);
@@ -34,8 +50,10 @@ function renderCart() {
     const image = cartItem.querySelector("[data-image]");
     image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`;
 
-    const quantity = cartItem.querySelector("[data-quantity]");
-    quantity.innerText = `x${entry.quantity}`;
+    if (entry.quantity > 1) {
+      const quantity = cartItem.querySelector("[data-quantity]");
+      quantity.innerText = `x${entry.quantity}`;
+    }
 
     const price = cartItem.querySelector("[data-price]");
     price.innerText = formatCurrency((item.priceCents * entry.quantity) / 100);
