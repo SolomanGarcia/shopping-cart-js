99f7975e469e40be488aeff22245e7b5f255db48
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-11 08:37:13 -0700

    create renderStoreItem function

--- store.js
+++ store.js
@@ -2,6 +2,7 @@ import items from "./items.json";
 
 const storeItemTemplate = document.querySelector("#store-item-template");
 const storeItemContainer = document.querySelector("[data-store-container]");
+const IMAGE_URL = "https://dummyimage.com/420x260";
 
 export function setupStore() {
   items.forEach(renderStoreItem);
@@ -15,4 +16,19 @@ function renderStoreItem(item) {
 
   const name = storeItem.querySelector("[data-name]");
   name.innerText = item.name;
+
+  const category = storeItem.querySelector("[data-category]");
+  category.innerText = item.category;
+
+  const image = storeItem.querySelector("[data-image]");
+  image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`;
+
+  const price = storeItem.querySelector("[data-price]");
+  const formatter = new Intl.NumberFormat(undefined, {
+    style: "currency",
+    currency: "USD"
+  });
+  price.innerText = formatter.format(item.priceCents / 100);
+
+  storeItemContainer.appendChild(storeItem);
 }
