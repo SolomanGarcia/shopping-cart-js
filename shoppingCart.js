9cd03fc48c2f52a042fefb34ef009141a6d584d4
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-11 09:50:13 -0700

    create shoppingCart.js

---
+++ shoppingCart.js
@@ -0,0 +1,10 @@
+export function setupShoppingCart() {}
+
+// Add items from cart
+// remove items from cart
+// show/hide the cart button when it has no items or when it goes from 0 to 1 item
+// Persist accross multiple pages
+// Calculate an accurate total
+// Handle multiple or the same item in the cart
+
+// show/hide the cart when clicked
